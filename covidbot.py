6bf2d484fb7b05ac85e52dc9d7cd0cf5175447c5
nboisclair64 <59835675+nboisclair64@users.noreply.github.com>
2020-07-11 16:38:17 -0400

    Update covidbot.py

--- covidbot.py
+++ covidbot.py
@@ -5,4 +5,6 @@ access_secret = '62eqCUBuELZzneZGrWm1P4hjJrBR0kK4aapLbpk4shH7Q'
 consumer_secret = 'uPaksCO6XJNt6coTyO4tg9KLBd3Aoapktafkw3y7lAtWKM8vBa'
 consumer_key = '0xe5EEREi3Y9SpxC0x1vXjDhp'
  
- 
+auth = tp.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+api = tp.API(auth)
