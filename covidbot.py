aeeebafc4c0c100103d437d135ea77b6adc83ddb
nboisclair64 <59835675+nboisclair64@users.noreply.github.com>
2020-07-11 16:31:39 -0400

    Update covidbot.py

--- covidbot.py
+++ covidbot.py
@@ -1,6 +1,8 @@
 import twitter 
 
-ACCESS_TOKEN = '1282041831409291267-TItwyHEvye8Doo4HLRVspUXVtXwsAu'
-ACCESS_SECRET = '62eqCUBuELZzneZGrWm1P4hjJrBR0kK4aapLbpk4shH7Q'
-CONSUMER_SECRET = 'uPaksCO6XJNt6coTyO4tg9KLBd3Aoapktafkw3y7lAtWKM8vBa'
-CONSUMER_KEY = '0xe5EEREi3Y9SpxC0x1vXjDhp'
+access_token = '1282041831409291267-TItwyHEvye8Doo4HLRVspUXVtXwsAu'
+access_secret = '62eqCUBuELZzneZGrWm1P4hjJrBR0kK4aapLbpk4shH7Q'
+consumer_secret = 'uPaksCO6XJNt6coTyO4tg9KLBd3Aoapktafkw3y7lAtWKM8vBa'
+consumer_key = '0xe5EEREi3Y9SpxC0x1vXjDhp'
+ 
+ 
